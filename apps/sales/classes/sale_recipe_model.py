60d4fe7301124ad21426a1e7df661d66995ae743
Daniel Fernandes <54218517+DaniFBN@users.noreply.github.com>
2021-11-14 16:51:56 -0300

    `SaleRecipe` tests

--- apps/sales/classes/sale_recipe_model.py
+++ apps/sales/classes/sale_recipe_model.py
@@ -1,24 +1,24 @@
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
-from apps.recipes.classes.recipe_model import Recipe
+from apps.recipes.classes.price_list_model import PriceList
 
 
 class SaleRecipe(models.Model):
     """ Contains a recipe and values to control price and profit of sale """
-    
-    recipe = models.ForeignKey(Recipe, on_delete=models.PROTECT)
-    
+
+    recipe = models.ForeignKey(PriceList, on_delete=models.PROTECT)
+
     # Amount of recipes sold
     amount = models.PositiveSmallIntegerField(default=1)
-    
+
     # Cost of an item of this sale
     item_cost = models.DecimalField(max_digits=6, decimal_places=2,
                                     validators=[MinValueValidator(0.01)]
                                     )
-    
+
     # Price of an item of this sale
-    item_value = models.DecimalField(max_digits=6, decimal_places=2,
+    item_price = models.DecimalField(max_digits=6, decimal_places=2,
                                      validators=[MinValueValidator(0.01)]
                                      )
 
@@ -28,15 +28,28 @@ class SaleRecipe(models.Model):
                                                     MaxValueValidator(100)]
                                                 )
 
+    @classmethod
+    def create(cls, data):
+        """ data = { recipe: PriceList, amount, item_price?, discount? } """
+        obj = cls(**data)
+        obj.item_cost = obj.recipe.cost
+
+        if not obj.item_price:
+            obj.item_price = obj.recipe.price
+
+        obj.save()
+
+        return obj
+
     @property
     def price(self):
         """ Calculate the price with discount of this sale """
-        price = self.item_value * self.amount
+        price = float(self.item_price * self.amount)
 
         discount = self.__calc_discount(price)
 
         final_price = price - discount
-        return final_price
+        return round(final_price, 2)
 
     @property
     def profit(self):
@@ -55,9 +68,10 @@ class SaleRecipe(models.Model):
 
     def __calc_discount(self, price):
         """ Calculate the discount value, if dont have discount, return 0 """
-        if not self.discount: return 0.0
-        
-        discount = price * (self.discount / 100)
+        if not self.discount:
+            return 0.0
+
+        discount = float(price) * (self.discount / 100)
         return round(discount, 2)
 
     def __calc_cost(self):
---
+++ apps/sales/tests/sale_recipe/create/tests.py
@@ -0,0 +1,45 @@
+from django.test import TestCase
+from model_bakery import baker
+
+from apps.sales.classes.sale_recipe_model import SaleRecipe
+
+
+class CreateSaleRecipeTestCase(TestCase):
+
+    def setUp(self):
+        packaging = baker.make('recipes.RecipePackaging', make_m2m=True)
+        recipe = baker.make('recipes.Recipe', packaging=packaging)
+        price_list = baker.make('recipes.PriceList', recipe=recipe,
+                                price=float(recipe.cost) * 2)
+
+        self.data = {'recipe': price_list, 'amount': 2}
+        self.discount_data = {**self.data, 'discount': 20}
+        self.price_data = {**self.data,
+                           'item_price': round(float(price_list.price) * 1.1, 2)}
+
+    def test_create_success(self):
+        response = SaleRecipe.create(self.data)
+
+        self.assertIn('id', response.__dict__)
+        price_list = self.data.get('recipe')
+        
+        self.assertEqual(response.profit, float(price_list.cost) * 2)
+        self.assertEqual(response.profit_percentage, 50)
+
+    def test_create_discount_success(self):
+        response = SaleRecipe.create(self.discount_data)
+
+        price = self.discount_data.get('recipe').price
+        price = round(float(price), 2) * 2
+
+        price_with_discount = round(price - (price * 0.2), 2)
+
+        self.assertEqual(price_with_discount, response.price)
+
+    def test_create_price_success(self):
+        response = SaleRecipe.create(self.price_data)
+
+        price = self.price_data.get('item_price')
+        new_price = round(price, 2)
+
+        self.assertEqual(new_price, response.item_price)
